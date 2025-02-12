cd425e2cb5ff284c6b932c09f9a58ef84345d798
staroneweb <ashish.joshi@staroneweb.co.in>
2025-02-12 10:05:51 +0530

    set middleware

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if(!Auth::guard('sanctum')->check()){
+            return response()->json(['code'=>500,'message' => 'Unauthenticated. Please log in.']);
+
+        }
+        return $next($request);
+    }
+}
